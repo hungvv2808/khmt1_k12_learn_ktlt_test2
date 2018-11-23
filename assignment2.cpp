d10549cb2bb71115059a65d4e3aaa7972b2746da
kingkong <vuhung2881999@gmail.com>
2018-11-23 22:46:04 +0700

    last commit

--- assignment2.cpp
+++ assignment2.cpp
@@ -63,7 +63,7 @@ void TwoLargestValue(double *a, int n){
 }
 
 bool ThreePositiveNumberConsecutively(double *a, int n){
-        for(int i=0; i<n; i++) {
+        for(int i=0; i<n-2; i++) {
                 if(a[i]>0 && a[i+1]>0 && a[i+2]>0) return true;
         }
         return false;
